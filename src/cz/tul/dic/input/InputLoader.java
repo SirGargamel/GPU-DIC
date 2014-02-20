4066d789498e3a423567147e949550c28360437f
Petr Ječmen <petr.jecmen@tul.cz>
2014-02-20 15:29:44 +0100

    Make loader picking more robust.

--- src/cz/tul/dic/input/InputLoader.java
+++ src/cz/tul/dic/input/InputLoader.java
@@ -12,23 +12,35 @@ import java.util.Map;
  * @author Petr Jecmen
  */
 public class InputLoader {
-    
+
     private static final Map<Class, IInputLoader> loaders;
-    
+
     static {
         loaders = new HashMap<>();
-        
+
         IInputLoader il = new VideoLoader();
         loaders.put(il.getSupporteType(), il);
-        
+
         il = new ImageLoader();
         loaders.put(il.getSupporteType(), il);
     }
-    
+
     public static void loadInput(final Object in, final TaskContainer tc) throws IOException {
         final Class cls = in.getClass();
+        IInputLoader loader = null;
+
         if (loaders.containsKey(cls)) {
-            final List<Image> images = loaders.get(cls).loadData(in, tc);
+            loader = loaders.get(cls);
+        } else {
+            for (Class c : loaders.keySet()) {
+                if (c.isAssignableFrom(cls)) {
+                    loader = loaders.get(c);
+                }
+            }
+        }
+
+        if (loader != null) {
+            final List<Image> images = loader.loadData(in, tc);
             for (int i = 0; i < images.size(); i++) {
                 tc.addImage(images.get(i));
             }
@@ -36,5 +48,5 @@ public class InputLoader {
             throw new IllegalArgumentException("Unsupported type of input data - " + cls.toString());
         }
     }
-    
+
 }
