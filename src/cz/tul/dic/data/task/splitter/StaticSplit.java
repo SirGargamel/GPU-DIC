9d525aea939b964e7ffb6a31485f924ca7834ee6
Petr Ječmen <petr.jecmen@tul.cz>
2014-03-06 09:27:23 +0100

    Fix static splitting to subtasks.

--- src/cz/tul/dic/data/task/splitter/StaticSplit.java
+++ src/cz/tul/dic/data/task/splitter/StaticSplit.java
@@ -10,37 +10,32 @@ import java.util.List;
 
 public class StaticSplit extends TaskSplitter {
 
-    private static final int SPLIT_DEFAULT = 10;
+    private static final int SPLIT_DEFAULT = 50;
 
     @Override
     public void split(TaskContainer tc) {
-        Object split = tc.getParameter(TaskParameter.TASK_SPLIT_VALUE);
+        final Object split = tc.getParameter(TaskParameter.TASK_SPLIT_VALUE);
         final int splitCount = split == null ? SPLIT_DEFAULT : (int) split;
         final int roundCount = TaskContainerUtils.getRoundCount(tc);
 
-        int facetCount, facetBase, facetSubtaskCount;
+        int facetCount, index;
         ComputationTask ct;
         List<Facet> sublist, facets;
         for (int round = 0; round < roundCount; round++) {
             facets = tc.getFacets(round);
             facetCount = facets.size();
-            facetSubtaskCount = facetCount / splitCount;
 
-            facetBase = 0;
-            for (int i = 0; i < splitCount - 1; i++) {
-                sublist = new ArrayList<>();
-                for (int f = facetBase; f < facetBase + facetSubtaskCount; f++) {
-                    sublist.add(facets.get(f));
-                }
-                ct = new ComputationTask(tc.getImage(round), tc.getImage(round + 1), sublist, tc.getDeformations());
-                tc.addTask(ct, round);
-
-                facetBase += facetSubtaskCount;
-            }
-
+            index = 0;
             sublist = new ArrayList<>();
-            for (int f = facetBase; f < facetCount; f++) {
-                sublist.add(facets.get(f));
+            while (index < facetCount) {
+                sublist.add(facets.get(index));
+                index++;
+
+                if (index % splitCount == 0) {
+                    ct = new ComputationTask(tc.getImage(round), tc.getImage(round + 1), sublist, tc.getDeformations());
+                    tc.addTask(ct, round);
+                    sublist = new ArrayList<>();
+                }
             }
             ct = new ComputationTask(tc.getImage(round), tc.getImage(round + 1), sublist, tc.getDeformations());
             tc.addTask(ct, round);
