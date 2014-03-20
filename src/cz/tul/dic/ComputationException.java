55d15ebcfbb230790d4fd5deb2b80aaa57daad22
Petr Ječmen <petr.jecmen@tul.cz>
2014-03-20 10:59:58 +0100

    Add custom exception class to signal errors during computation which user can fix by changing input parameters.

---
+++ src/cz/tul/dic/ComputationException.java
@@ -0,0 +1,34 @@
+package cz.tul.dic;
+
+/**
+ *
+ * @author Petr Jecmen
+ */
+public class ComputationException extends Exception {
+    
+    private final ComputationExceptionCause cause;
+
+    /**
+     * Creates a new instance of <code>ComputationException</code> without
+     * detail message.
+     * @param cause
+     */
+    public ComputationException(final ComputationExceptionCause cause) {
+        super();
+        
+        this.cause = cause;
+    }
+
+    /**
+     * Constructs an instance of <code>ComputationException</code> with the
+     * specified detail message.
+     *
+     * @param cause
+     * @param msg the detail message.
+     */
+    public ComputationException(final ComputationExceptionCause cause, final String msg) {
+        super(msg);
+        
+        this.cause = cause;
+    }
+}
---
+++ src/cz/tul/dic/ComputationExceptionCause.java
@@ -0,0 +1,12 @@
+package cz.tul.dic;
+
+
+/**
+ *
+ * @author Petr Jecmen
+ */
+public enum ComputationExceptionCause {
+
+    NOT_ENOUGH_ROIS,;
+
+}
