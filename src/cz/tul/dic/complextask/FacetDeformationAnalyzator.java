0d2aeb9c1828b90a1600cca0817b5e8d49caa378
Petr Ječmen <petr.jecmen@tul.cz>
2014-03-25 10:01:17 +0100

    Analyza posunu celisti na zaklade max. poctu, ne prumerovani s presnosti na jedno desetinne misto. closes #72.

--- src/cz/tul/dic/complextask/FacetDeformationAnalyzator.java
+++ src/cz/tul/dic/complextask/FacetDeformationAnalyzator.java
@@ -18,19 +18,19 @@ public class FacetDeformationAnalyzator {
         final int facetSize = tc.getFacetSize(round, roi);
         final int facetSize2 = facetSize * facetSize;
 
-        int[] counterPos = new int[10];
-        int[] counterNeg = new int[10];
+        int[] counterPos = new int[50];
+        int[] counterNeg = new int[50];
 
         int val;
         int[] facetData;
         for (Facet f : facets) {
             facetData = f.getData();
             for (int i = 0; i < facetSize2; i++) {
-                val = (int) Math.round(results[facetData[i * 2]][facetData[i * 2 + 1]][Coordinates.Y]);
+                val = (int) Math.round(10 * results[facetData[i * 2]][facetData[i * 2 + 1]][Coordinates.Y]);
                 if (val >= 0) {
                     if (val >= counterPos.length) {
                         int[] tmp = counterPos;
-                        counterPos = new int[val+1];
+                        counterPos = new int[val + 1];
                         System.arraycopy(tmp, 0, counterPos, 0, tmp.length);
                     }
 
@@ -40,7 +40,7 @@ public class FacetDeformationAnalyzator {
 
                     if (val >= counterNeg.length) {
                         int[] tmp = counterNeg;
-                        counterNeg = new int[val+1];
+                        counterNeg = new int[val + 1];
                         System.arraycopy(tmp, 0, counterNeg, 0, tmp.length);
                     }
 
@@ -57,7 +57,7 @@ public class FacetDeformationAnalyzator {
                 val = i;
             }
         }
-        
+
         for (int i = 0; i < counterNeg.length; i++) {
             if (counterNeg[i] > count) {
                 count = counterNeg[i];
@@ -65,23 +65,25 @@ public class FacetDeformationAnalyzator {
             }
         }
         
-        double result = 0, tmp;
-        count = 0;
-        for (Facet f : facets) {
-            facetData = f.getData();
-            for (int i = 0; i < facetSize2; i++) {
-                tmp = results[facetData[i * 2]][facetData[i * 2 + 1]][Coordinates.Y];
-                if (tmp >= val && (tmp - val) < 0.5) {
-                    result += tmp;
-                    count++;
-                } else if (tmp < val && (val - tmp) <= 0.5) {
-                    result += tmp;
-                    count++;
-                }
-            }
-        }
+        return val / 10.0;
 
-        return result / (double) count;
+//        double result = 0, tmp;
+//        count = 0;
+//        for (Facet f : facets) {
+//            facetData = f.getData();
+//            for (int i = 0; i < facetSize2; i++) {
+//                tmp = results[facetData[i * 2]][facetData[i * 2 + 1]][Coordinates.Y];
+//                if (tmp >= val && (tmp - val) < 0.5) {
+//                    result += tmp;
+//                    count++;
+//                } else if (tmp < val && (val - tmp) <= 0.5) {
+//                    result += tmp;
+//                    count++;
+//                }
+//            }
+//        }
+
+//        return result / (double) count;
     }
 
 }
