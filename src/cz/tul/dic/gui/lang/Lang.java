2c5569663138e8a088ffadf4c99e4abac2b7f900
Petr Ječmen <petr.jecmen@tul.cz>
2014-03-28 09:33:57 +0100

    Provide text line fo rmissing resource lines.

--- src/cz/tul/dic/gui/lang/Lang.java
+++ src/cz/tul/dic/gui/lang/Lang.java
@@ -1,6 +1,7 @@
 package cz.tul.dic.gui.lang;
 
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
@@ -8,25 +9,31 @@ import java.util.ResourceBundle;
  * @author Petr Jecmen
  */
 public class Lang {
-    
+
+    private static final String MISSING_RESOURCE = "MISSING";
     private static final ResourceBundle rb;
-    
+
     static {
         rb = ResourceBundle.getBundle("cz.tul.dic.gui.lang.Lang", Locale.getDefault());
     }
-    
+
     public static ResourceBundle getBundle() {
         return rb;
     }
-    
+
     public static String getString(final String key, String... params) {
-        String result = rb.getString(key).replaceAll("\\\\n", "\n");
-        
-        for (int i = 0; i < params.length; i++) {
-            result = result.replaceAll("\\{".concat(Integer.toString(i).concat("\\}")), params[i]);
+        String result;
+        try {
+            result = rb.getString(key).replaceAll("\\\\n", "\n");
+
+            for (int i = 0; i < params.length; i++) {
+                result = result.replaceAll("\\{".concat(Integer.toString(i).concat("\\}")), params[i]);
+            }
+        } catch (MissingResourceException ex) {
+            result = MISSING_RESOURCE;
         }
-        
+
         return result;
-    }        
-    
+    }
+
 }
