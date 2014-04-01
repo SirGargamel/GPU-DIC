44b2d6b06f3d656a58e5dbe583ddfacd1060bde2
Petr Ječmen <petr.jecmen@tul.cz>
2014-04-01 12:16:23 +0200

    Show dialog when loading data to show progress.

--- src/cz/tul/dic/gui/MainWindow.java
+++ src/cz/tul/dic/gui/MainWindow.java
@@ -4,19 +4,11 @@ import cz.tul.dic.Computation;
 import cz.tul.dic.ComputationException;
 import cz.tul.dic.data.Config;
 import cz.tul.dic.data.ConfigType;
-import cz.tul.dic.data.roi.CircularROI;
-import cz.tul.dic.data.roi.ROI;
 import cz.tul.dic.data.task.TaskContainer;
-import cz.tul.dic.data.task.TaskContainerChecker;
 import cz.tul.dic.data.task.TaskContainerUtils;
-import cz.tul.dic.data.task.TaskParameter;
 import cz.tul.dic.gui.lang.Lang;
 import cz.tul.dic.input.InputLoader;
-import cz.tul.dic.output.Direction;
-import cz.tul.dic.output.ExportMode;
-import cz.tul.dic.output.ExportTarget;
 import cz.tul.dic.output.ExportTask;
-import cz.tul.dic.output.OutputUtils;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -24,6 +16,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -56,7 +50,7 @@ public class MainWindow implements Initializable {
     private Button buttonExpert;
 
     @FXML
-    private void handleButtonActionInput(ActionEvent event) throws IOException {
+    private void handleButtonActionInput(ActionEvent event) throws IOException, InterruptedException, ExecutionException {
         final FileChooser fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(DEFAULT_DIR);
         fileChooser.getExtensionFilters().clear();
@@ -66,62 +60,81 @@ public class MainWindow implements Initializable {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Task files (*.task)", "*.task"));
         List<File> fileList = fileChooser.showOpenMultipleDialog(null);
         if (fileList != null && !fileList.isEmpty()) {
-            if (fileList.size() == 1) {
-                final File in = fileList.get(0);
-                final String name = in.getName();
-                final String ext = name.substring(name.lastIndexOf(".") + 1);
-                switch (ext) {
-                    case "avi":
-                        Context.getInstance().setTc(new TaskContainer(in));
-                        break;
-                    case "config":
-                        final ConfigType ct = Config.determineType(in);
-                        switch (ct) {
-                            case TASK:
-                                Context.getInstance().setTc(TaskContainerUtils.deserializeTaskContainerFromConfig(in));
+            Task<Object> worker = new Task<Object>() {
+                @Override
+                protected Object call() throws Exception {
+                    updateProgress(0, 5);
+                    if (fileList.size() == 1) {
+                        final File in = fileList.get(0);
+                        final String name = in.getName();
+                        final String ext = name.substring(name.lastIndexOf(".") + 1);
+                        switch (ext) {
+                            case "avi":
+                                updateProgress(1, 5);
+                                Context.getInstance().setTc(new TaskContainer(in));
                                 break;
-                            case EXPORT:
-                                exports = OutputUtils.deserializeExports(in);
+                            case "config":
+                                updateProgress(1, 5);
+                                final ConfigType ct = Config.determineType(in);
+                                switch (ct) {
+                                    case TASK:
+                                        Context.getInstance().setTc(TaskContainerUtils.deserializeTaskContainerFromConfig(in));
+                                        break;
+                                    case SEQUENCE:
+                                        // find avi and load it                                                                 
+                                        Context.getInstance().setTc(new TaskContainer(Config.determineProjectFile(in)));
+                                }
                                 break;
-                            case SEQUENCE:
-                                // find avi and load it                                
-                                Context.getInstance().setTc(new TaskContainer(Config.determineProjectFile(in)));
+                            case "task":
+                                updateProgress(1, 5);
+                                try {
+                                    Context.getInstance().setTc(TaskContainerUtils.readTaskFromFile(in));
+                                } catch (ClassNotFoundException | IOException ex) {
+                                    Dialogs.create()
+                                            .title(Lang.getString("error"))
+                                            .message(Lang.getString("wrongBin"))
+                                            .showWarning();
+                                }
+                                break;
+                            default:
+                                Dialogs.create()
+                                        .title(Lang.getString("error"))
+                                        .message(Lang.getString("wrongIn"))
+                                        .showWarning();
                         }
-
-                        break;
-                    case "task":
-                        try {
-                            Context.getInstance().setTc(TaskContainerUtils.readTaskFromFile(in));
-                        } catch (ClassNotFoundException | IOException ex) {
-                            // TODO show error during loading
-                            Dialogs.create()
-                                    .title(Lang.getString("error"))
-                                    .message(Lang.getString("wrongBin"))
-                                    .showWarning();
-                        }
-                        break;
-                    default:
+                        updateProgress(2, 5);
+                    } else {
+                        updateProgress(1, 5);
+                        Context.getInstance().setTc(new TaskContainer(fileList));
+                        updateProgress(2, 5);
+                    }
+                    try {
+                        InputLoader.loadInput(Context.getInstance().getTc());
+                        updateProgress(4, 5);
+                        buttonExpert.setDisable(false);
+                        buttonROI.setDisable(false);
+                        buttonRun.setDisable(false);
+                        textFs.setDisable(false);
+                    } catch (IOException ex) {
                         Dialogs.create()
                                 .title(Lang.getString("error"))
-                                .message(Lang.getString("wrongIn"))
+                                .message(Lang.getString("IO", ex.getLocalizedMessage()))
                                 .showWarning();
+                    }
+                    updateProgress(5, 5);
+                    return null;
                 }
+            };
 
-            } else {
-                Context.getInstance().setTc(new TaskContainer(fileList));
-            }
-            try {
-                InputLoader.loadInput(Context.getInstance().getTc());
-                buttonExpert.setDisable(false);
-                buttonROI.setDisable(false);
-                buttonRun.setDisable(false);
-                textFs.setDisable(false);
-            } catch (IOException ex) {
-                Dialogs.create()
-                        .title(Lang.getString("error"))
-                        .message(Lang.getString("IO", ex.getLocalizedMessage()))
-                        .showWarning();
-            }
+            Dialogs.create()
+                    .title(Lang.getString("Wait"))
+                    .message(Lang.getString("LoadingData"))
+                    .masthead(null)                    
+                    .showWorkerProgress(worker);
+
+            Thread th = new Thread(worker);
+            th.setDaemon(true);
+            th.start();
         }
     }
 
@@ -163,42 +176,64 @@ public class MainWindow implements Initializable {
 
     @FXML
     private void handleButtonActionExpert(ActionEvent event) throws ComputationException, IOException {
-        TaskContainer tc = new TaskContainer(new File("D:\\temp\\7202845m.avi"));
-        InputLoader.loadInput(tc);
+        Task<Object> worker = new Task<Object>() {
+            @Override
+            protected Object call() throws Exception {
+                for (int i = 0; i < 100; i++) {
+                    updateProgress(i, 99);
+                    updateMessage("progress: " + i);
+                    System.out.println("progress: " + i);
+                    Thread.sleep(100);
+                }
+                return null;
+            }
+        };
 
-        final int roiRadius = 26;
-        tc.addRoi(new CircularROI(108, 12, roiRadius), 0);
-        tc.addRoi(new CircularROI(201, 7, roiRadius), 0);
-        tc.addRoi(new CircularROI(108, 86, roiRadius), 0);
-        tc.addRoi(new CircularROI(202, 84, roiRadius), 0);
+        Dialogs.create()
+                .title("Progress")
+                .message("Now Loading...")
+                .showWorkerProgress(worker);
 
-        for (ROI roi : tc.getRois(0)) {
-            tc.setDeformationLimits(new double[]{-1, 1, 1.0, -5, 5, 0.25}, 0, roi);
-        }
+        Thread th = new Thread(worker);
+        th.setDaemon(true);
+        th.start();
 
-        TaskContainerUtils.setUniformFacetSize(tc, 0, roiRadius / 2);
-        TaskContainerChecker.checkTaskValidity(tc);
-
-        final String target = new File("D:\\temp\\results").getAbsolutePath().concat(File.separator).concat("dyn").concat(File.separator).concat(tc.getParameter(TaskParameter.KERNEL).toString()).concat("-");
-        final String ext = String.format("%02d", 19).concat(".bmp");
-        for (int round = 0; round < TaskContainerUtils.getRoundCount(tc); round++) {
-            exports.add(new ExportTask(ExportMode.MAP, ExportTarget.FILE, Direction.X, new File(target.concat(String.format("%02d", round)).concat("-X-").concat(ext)), new int[]{round}));
-            exports.add(new ExportTask(ExportMode.MAP, ExportTarget.FILE, Direction.Y, new File(target.concat(String.format("%02d", round)).concat("-Y-").concat(ext)), new int[]{round}));
-            exports.add(new ExportTask(ExportMode.MAP, ExportTarget.FILE, Direction.ABS, new File(target.concat(String.format("%02d", round)).concat("-ABS-").concat(ext)), new int[]{round}));
-        }
-        exports.add(new ExportTask(ExportMode.SEQUENCE, ExportTarget.FILE, Direction.X, new File(target.concat("-X-").concat(ext).replace("bmp", "avi")), null));
-        exports.add(new ExportTask(ExportMode.SEQUENCE, ExportTarget.FILE, Direction.Y, new File(target.concat("-Y-").concat(ext).replace("bmp", "avi")), null));
-
-        TaskContainerUtils.serializeTaskContainerToConfig(tc);
-        OutputUtils.serializeExports(exports, tc);
-        
-        // compute dynamic task
-        Computation.computeDynamicTask(tc);
-        
-        // serialize task container to binary file
-        TaskContainerUtils.dumpTaskToFile(new File("D:\\temp\\task.task"), tc);
+//        TaskContainer tc = new TaskContainer(new File("D:\\temp\\7202845m.avi"));
+//        InputLoader.loadInput(tc);
+//
+//        final int roiRadius = 26;
+//        tc.addRoi(new CircularROI(108, 12, roiRadius), 0);
+//        tc.addRoi(new CircularROI(201, 7, roiRadius), 0);
+//        tc.addRoi(new CircularROI(108, 86, roiRadius), 0);
+//        tc.addRoi(new CircularROI(202, 84, roiRadius), 0);
+//
+//        for (ROI roi : tc.getRois(0)) {
+//            tc.setDeformationLimits(new double[]{-1, 1, 1.0, -5, 5, 0.25}, 0, roi);
+//        }
+//
+//        TaskContainerUtils.setUniformFacetSize(tc, 0, roiRadius / 2);
+//        TaskContainerChecker.checkTaskValidity(tc);
+//
+//        final String target = new File("D:\\temp\\results").getAbsolutePath().concat(File.separator).concat("dyn").concat(File.separator).concat(tc.getParameter(TaskParameter.KERNEL).toString()).concat("-");
+//        final String ext = String.format("%02d", 19).concat(".bmp");
+//        for (int round = 0; round < TaskContainerUtils.getRoundCount(tc); round++) {
+//            exports.add(new ExportTask(ExportMode.MAP, ExportTarget.FILE, Direction.X, new File(target.concat(String.format("%02d", round)).concat("-X-").concat(ext)), new int[]{round}));
+//            exports.add(new ExportTask(ExportMode.MAP, ExportTarget.FILE, Direction.Y, new File(target.concat(String.format("%02d", round)).concat("-Y-").concat(ext)), new int[]{round}));
+//            exports.add(new ExportTask(ExportMode.MAP, ExportTarget.FILE, Direction.ABS, new File(target.concat(String.format("%02d", round)).concat("-ABS-").concat(ext)), new int[]{round}));
+//        }
+//        exports.add(new ExportTask(ExportMode.SEQUENCE, ExportTarget.FILE, Direction.X, new File(target.concat("-X-").concat(ext).replace("bmp", "avi")), null));
+//        exports.add(new ExportTask(ExportMode.SEQUENCE, ExportTarget.FILE, Direction.Y, new File(target.concat("-Y-").concat(ext).replace("bmp", "avi")), null));
+//
+//        TaskContainerUtils.serializeTaskContainerToConfig(tc);
+//        OutputUtils.serializeExports(exports, tc);
+//
+//        // compute dynamic task
+//        Computation.computeDynamicTask(tc);
+//
+//        // serialize task container to binary file
+//        TaskContainerUtils.dumpTaskToFile(new File("D:\\temp\\task.task"), tc);
     }
-    
+
     @FXML
     private void handleButtonActionResults(ActionEvent event) {
         try {
@@ -216,7 +251,7 @@ public class MainWindow implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         exports = new HashSet<>();
 
-        buttonExpert.setDisable(true);
+        buttonExpert.setDisable(false);
         buttonROI.setDisable(true);
         buttonRun.setDisable(true);
 
