0a457e696e1cc308a6dc4126e9cd1ab74fe95e69
Petr Ječmen <petr.jecmen@tul.cz>
2014-04-03 15:06:12 +0200

    Show dialog with deformations in that point for all rounds. closes #49.

--- src/cz/tul/dic/gui/ResultPresenter.java
+++ src/cz/tul/dic/gui/ResultPresenter.java
@@ -13,14 +13,26 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 import javafx.util.Duration;
+import org.controlsfx.dialog.Dialog;
 
 public class ResultPresenter implements Initializable {
-    
+
+    private static final int PREF_SIZE_W_BASE = 30;
+    private static final int PREF_SIZE_W_M = 5;
+    private static final int PREF_SIZE_H = 30;
+
     @FXML
     private ComboBox<Direction> choiceDir;
     @FXML
@@ -123,10 +135,54 @@ public class ResultPresenter implements Initializable {
         choiceDir.setItems(comboBoxData);
 
         choiceDir.getSelectionModel().selectFirst();
-        
+
         image.setPreserveRatio(true);
         image.setFitHeight(0);
         image.setFitWidth(0);
+
+        image.setOnMouseClicked((MouseEvent t) -> {
+            final int x = (int) Math.round(t.getX());
+            final int y = (int) Math.round(t.getY());
+            final double[] line = Context.getInstance().getLineResult(x, y, choiceDir.getValue());
+
+            final Axis xAxis = new NumberAxis(0, line.length, 1);
+            final Axis yAxis = new NumberAxis();
+            final LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+            chart.setLegendVisible(false);
+            final double width = PREF_SIZE_W_BASE + line.length * PREF_SIZE_W_M;
+            chart.setPrefSize(width, PREF_SIZE_H);
+
+            XYChart.Series series = new XYChart.Series();
+            double min = Double.MAX_VALUE, max = -Double.MAX_VALUE;
+            for (int i = 0; i < line.length; i++) {
+                series.getData().add(new XYChart.Data(i, line[i]));
+                if (line[i] < min) {
+                    min = line[i];
+                }
+                if (line[i] > max) {
+                    max = line[i];
+                }
+            }
+            chart.getData().add(series);
+
+            series = new XYChart.Series();
+            series.getData().add(new XYChart.Data(index, min));
+            series.getData().add(new XYChart.Data(index, max));
+            chart.getData().add(series);
+
+            final Dialog d = new Dialog(image, String.format("%1d : %2d", x, y));
+            d.contentProperty().setValue(chart);
+            d.mastheadProperty().setValue(null);
+            d.setResizable(true);
+
+            final Window w = d.getWindow();
+            w.setOnShown((WindowEvent t2) -> {
+                w.setX(w.getX() + x + width / 2 + 10);
+                w.setY(w.getY() + y + PREF_SIZE_H / 2 + 10);
+            });            
+
+            d.show();            
+        });
     }
 
 }
