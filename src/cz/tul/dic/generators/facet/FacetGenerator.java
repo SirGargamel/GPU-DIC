312df84fae03c377ff1c1083a7425ebaa05348f6
Petr Ječmen <petr.jecmen@tul.cz>
2014-04-09 11:11:07 +0200

    Forgotten exception throwing.

--- src/cz/tul/dic/generators/facet/FacetGenerator.java
+++ src/cz/tul/dic/generators/facet/FacetGenerator.java
@@ -1,5 +1,6 @@
 package cz.tul.dic.generators.facet;
 
+import cz.tul.dic.ComputationException;
 import cz.tul.dic.data.task.TaskContainer;
 import cz.tul.dic.data.task.TaskParameter;
 import java.util.HashMap;
@@ -22,7 +23,7 @@ public class FacetGenerator {
         generators.put(fg.getMode(), fg);
     }
 
-    public static void generateFacets(final TaskContainer tc, final int round) {
+    public static void generateFacets(final TaskContainer tc, final int round) throws ComputationException {
         final FacetGeneratorMode mode = (FacetGeneratorMode) tc.getParameter(TaskParameter.FACET_GENERATOR_MODE);
         if (generators.containsKey(mode)) {            
             generators.get(mode).generateFacets(tc, round);
