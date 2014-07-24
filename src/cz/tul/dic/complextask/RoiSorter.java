79629a867452a384c923b1429850506bae8e0107
Petr Ječmen <petr.jecmen@tul.cz>
2014-07-24 09:21:32 +0200

    Fix for cases when one of the ROIs are next to each other and one is both above and below the other one.

--- src/cz/tul/dic/complextask/RoiSorter.java
+++ src/cz/tul/dic/complextask/RoiSorter.java
@@ -2,6 +2,7 @@ package cz.tul.dic.complextask;
 
 import cz.tul.dic.data.roi.ROI;
 import java.util.Comparator;
+import org.pmw.tinylog.Logger;
 
 /**
  *
@@ -13,16 +14,25 @@ public class RoiSorter implements Comparator<ROI> {
     public int compare(ROI o1, ROI o2) {
         final int y11 = o1.getY1();
         final int y12 = o1.getY2();
+        final int center1 = (y11 + y12) / 2;
         final int y21 = o2.getY1();
         final int y22 = o2.getY2();
 
         final int result;
-        if (y11 >= y21 && y11 <= y22 || y12 >= y21 && y12 <= y22) {
+        if (y11 >= y21 && y11 <= y22 || y12 >= y21 && y12 <= y22 || center1 >= y21 && center1 <= y22) {
             result = Integer.compare(o1.getX1(), o2.getX1());
         } else {
             result = Integer.compare(y11, y21);
-        }        
-
+        }     
+        
+        final StringBuilder sb = new StringBuilder();
+        sb.append(o1);
+        sb.append(" vs ");
+        sb.append(o2);
+        sb.append(" - ");
+        sb.append(result);
+        Logger.trace(sb);
+        
         return result;
     }
 
