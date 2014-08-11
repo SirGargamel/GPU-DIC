52ac84696de1a46dcfc4ca5ccb3f6d814e4046c5
Petr Ječmen <petr.jecmen@tul.cz>
2014-08-11 12:30:06 +0200

    Fix cumulative result couting to be really cumulative for all rounds. closes #182.

--- src/cz/tul/dic/engine/CumulativeResultsCounter.java
+++ src/cz/tul/dic/engine/CumulativeResultsCounter.java
@@ -1,10 +1,7 @@
 package cz.tul.dic.engine;
 
-import cz.tul.dic.data.Coordinates;
 import cz.tul.dic.data.task.TaskContainer;
-import cz.tul.dic.data.task.TaskContainerUtils;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,45 +11,35 @@ import java.util.List;
 public class CumulativeResultsCounter {
 
     public static List<double[][][]> calculate(final TaskContainer tc, final List<double[][][]> data) {
-        final int roundCount = TaskContainerUtils.getMaxRoundCount(tc);
-        final int srcPos = TaskContainerUtils.getRounds(tc).keySet().iterator().next();
-        int destPos = srcPos;
-        for (Integer i : TaskContainerUtils.getRounds(tc).keySet()) {
-            if (i > destPos) {
-                destPos = i;
-            }
-        }
-        return calculateCumulativeResults(data, roundCount, srcPos, destPos);
-    }
+        final List<double[][][]> result = new ArrayList<>(data.size());
 
-    private static List<double[][][]> calculateCumulativeResults(final List<double[][][]> data, final int count, final int srcPos, final int destPos) {
-        final List<double[][][]> result = new ArrayList<>(count);
-        for (int i = 0; i < count; i++) {
-            result.add(null);
-        }
-
-        for (int i = srcPos; i <= destPos; i++) {
-            result.set(i, calculateCumulativeResult(data, srcPos, i));
+        for (int i = 0; i < data.size(); i++) {
+            result.add(calculateCumulativeResult(data, 0, i));
         }
 
         return result;
     }
 
-    private static double[][][] calculateCumulativeResult(final List<double[][][]> data, final int srcPos, final int destPos) {
-        double[][][] roundData = data.get(srcPos);        
-        final double[][][] result = new double[roundData.length][roundData[0].length][];
-
+    public static double[][][] calculateCumulativeResult(final List<double[][][]> data, final int srcPos, final int destPos) {
+        double[][][] result = null;
+        double[][][] roundData;
         int l;
         for (int i = srcPos; i <= destPos; i++) {
             roundData = data.get(i);
 
-            for (int x = 0; x < result.length; x++) {
-                for (int y = 0; y < result[x].length; y++) {
-                    if (roundData[x][y] != null) {
-                        l = roundData[x][y].length;
-                        result[x][y] = new double[l];
-                        for (int j = 0; j < l; j++) {
-                            result[x][y][j] += roundData[x][y][j];
+            if (roundData != null) {
+                if (result == null) {
+                    result = new double[roundData.length][roundData[0].length][];
+                }
+
+                for (int x = 0; x < result.length; x++) {
+                    for (int y = 0; y < result[x].length; y++) {
+                        if (roundData[x][y] != null) {
+                            l = roundData[x][y].length;
+                            result[x][y] = new double[l];
+                            for (int j = 0; j < l; j++) {
+                                result[x][y][j] += roundData[x][y][j];
+                            }
                         }
                     }
                 }
