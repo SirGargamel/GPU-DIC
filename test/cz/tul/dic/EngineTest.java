580a191d6aef73c16b235425a7aafa9b3bef1ce4
Petr Ječmen <petr.jecmen@tul.cz>
2014-08-21 14:14:44 +0200

    Add test for computation with multiple facets. closes #194.

--- test/cz/tul/dic/EngineTest.java
+++ test/cz/tul/dic/EngineTest.java
@@ -1,16 +1,21 @@
 package cz.tul.dic;
 
+import cz.tul.dic.data.Facet;
 import cz.tul.dic.data.Image;
+import cz.tul.dic.data.deformation.DeformationUtils;
 import cz.tul.dic.data.roi.ROI;
 import cz.tul.dic.data.roi.RectangleROI;
 import cz.tul.dic.data.task.TaskContainer;
 import cz.tul.dic.data.task.TaskContainerUtils;
 import cz.tul.dic.data.task.TaskParameter;
 import cz.tul.dic.data.task.splitter.TaskSplitMethod;
+import cz.tul.dic.engine.CorrelationCalculator;
 import cz.tul.dic.engine.CumulativeResultsCounter;
 import cz.tul.dic.engine.Engine;
+import cz.tul.dic.engine.displacement.DisplacementCalculator;
 import cz.tul.dic.engine.opencl.KernelType;
 import cz.tul.dic.engine.opencl.interpolation.Interpolation;
+import cz.tul.dic.generators.facet.FacetGenerator;
 import cz.tul.dic.generators.facet.FacetGeneratorMethod;
 import cz.tul.dic.input.InputLoader;
 import java.io.File;
@@ -345,5 +350,56 @@ public class EngineTest {
         }
 
         return result;
+    }
+
+    @Test
+    public void testMultiFacet() throws IOException, URISyntaxException, ComputationException {
+        final List<File> input = new ArrayList<>(2);
+        input.add(Paths.get(getClass().getResource("/resources/in.bmp").toURI()).toFile());
+        input.add(Paths.get(getClass().getResource("/resources/" + DEF_ZERO_FIRST_SECOND_FILES[0] + ".bmp").toURI()).toFile());
+
+        final TaskContainer tc = new TaskContainer(input);
+        InputLoader.loadInput(tc);
+
+        final ROI roi = new RectangleROI(85, 85, 95, 95);
+        final int fs = 11;
+
+        tc.addRoi(ROUND, roi);
+        tc.setDeformationLimits(ROUND, roi, DEF_SECOND_F);
+        tc.setParameter(TaskParameter.FACET_SIZE, fs);
+
+        TaskContainerUtils.checkTaskValidity(tc);
+
+        final CorrelationCalculator correlation = new CorrelationCalculator();
+        correlation.setKernel((KernelType) tc.getParameter(TaskParameter.KERNEL));
+        correlation.setInterpolation((Interpolation) tc.getParameter(TaskParameter.INTERPOLATION));
+        final TaskSplitMethod taskSplit = (TaskSplitMethod) tc.getParameter(TaskParameter.TASK_SPLIT_METHOD);
+        correlation.setTaskSplitVariant(taskSplit);
+        final Object taskSplitValue = new Object[]{
+            TaskContainerUtils.getDeformationArrayLength(tc, ROUND, roi),
+            tc.getFacetSize(ROUND, roi)};
+        
+        Map<ROI, List<Facet>> facets = new HashMap<>(1);
+        final List<Facet> roiFacets = new ArrayList<>(4);
+        roiFacets.add(Facet.createFacet(11, roi.getX1(), roi.getY1()));
+        roiFacets.add(Facet.createFacet(11, roi.getX1(), roi.getY1()));
+        roiFacets.add(Facet.createFacet(11, roi.getX1(), roi.getY1()));
+        roiFacets.add(Facet.createFacet(11, roi.getX1(), roi.getY1()));
+        facets.put(roi, roiFacets);
+
+        tc.setResult(
+                ROUND,
+                roi,
+                correlation.computeCorrelations(
+                        tc.getImage(ROUND), tc.getImage(ROUND + 1),
+                        roi, roiFacets,
+                        tc.getDeformationLimits(ROUND, roi),
+                        DeformationUtils.getDegreeFromLimits(tc.getDeformationLimits(ROUND, roi)),
+                        TaskContainerUtils.getDeformationArrayLength(tc, ROUND, roi),
+                        tc.getFacetSize(ROUND, roi), taskSplitValue));
+        
+        DisplacementCalculator.computeDisplacement(tc, ROUND, facets);
+
+        Assert.assertNull(checkResultsBack(tc, DEF_ZERO_FIRST_SECOND_FILES[0]));
     }
 }
