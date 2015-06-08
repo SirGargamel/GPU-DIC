7a4b910ca090847ba4e0b29616aae04e1a77b98d
Petr Ječmen <petr.jecmen@tul.cz>
2015-06-08 15:28:53 +0200

    Code refactoring and improvement.

--- src/cz/tul/dic/data/task/splitter/NoSplit.java
+++ src/cz/tul/dic/data/task/splitter/NoSplit.java
@@ -9,24 +9,28 @@ import cz.tul.dic.data.Facet;
 import cz.tul.dic.data.Image;
 import cz.tul.dic.data.task.ComputationTask;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class NoSplit extends TaskSplitter {
 
-    private boolean hasNext;
+    private boolean hasNextElement;
 
-    public NoSplit(Image image1, Image image2, final List<Facet> facets, final List<double[]> deformationLimits) {
+    public NoSplit(final Image image1, final Image image2, final List<Facet> facets, final List<double[]> deformationLimits) {
         super(image1, image2, facets, deformationLimits);
-        hasNext = true;
+        hasNextElement = true;
     }
 
     @Override
     public boolean hasNext() {
-        return hasNext;
+        return hasNextElement;
     }
 
     @Override
     public ComputationTask next() {
-        hasNext = false;
+        if (hasNext()) {
+            throw new NoSuchElementException();
+        }
+        hasNextElement = false;
         return new ComputationTask(image1, image2, facets, deformationLimits, false);
     }
 
@@ -35,17 +39,25 @@ public class NoSplit extends TaskSplitter {
         throw new UnsupportedOperationException("Not supported.");
     }
 
+    /**
+     * Not available since the task size is always maximal.
+     */
     @Override
     public void signalTaskSizeTooBig() {
+        throw new UnsupportedOperationException("No task division available.");
     }
 
     @Override
     public boolean isSplitterReady() {
-        return hasNext;
+        return hasNextElement;
     }
 
+    /**
+     * Not available since the task size is always maximal.
+     */
     @Override
-    public void resetTaskSize() {        
+    public void resetTaskSize() {
+        throw new UnsupportedOperationException("No task division available.");
     }
 
 }
